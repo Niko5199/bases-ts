d771191ddfb1273f761800da3ab28c450e07082c
JARP <paredesjosue77@gmail.com>
2021-12-30 19:28:21 -0600

    Seccion 3: Tipos Basicos

---
+++ tipos/01-booleans.js
@@ -0,0 +1,8 @@
+"use strict";
+(function () {
+    let isSuperman = true;
+    let isBatman = false;
+    //* Podemos usar operadores lógicos para combinar booleanos
+    isSuperman = true && false;
+    console.log({ isSuperman });
+})();
---
+++ tipos/09-never.js
@@ -0,0 +1,26 @@
+"use strict";
+(() => {
+    /*
+     * El tipo de dato never es un tipo de dato never es never no hay que
+     * confundir que es null o undefined.
+     *
+     * En pocas palabras el never significa que mi funcion no debe determinar
+     * existosamente, basicamente cuando veamos que el tipo de retorno sea
+     * never, significa que la funcion terminara con nuestro codigo.
+     *
+     * Ahora en dado caso que deseemos retornar un numero en nuestra funcion
+     * en caso de que no se cumpla este error simplemente asignemos otro valor
+     * de retorno y simplemente agregamos el valor de retorno que puede
+     * retornar nuestra funcion.
+     *
+     */
+    const error = (message) => {
+        throw new Error(`Error: ${message}`);
+    };
+    const error2 = (message) => {
+        if (false)
+            throw new Error(`Error: ${message}`);
+        return 1;
+    };
+    error('Ocurrio un error');
+})();
